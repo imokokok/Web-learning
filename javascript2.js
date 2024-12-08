6174d7538ec63d19f3aa8b39d42b9dfa17fec7e2
imokokok <145034722+imokokok@users.noreply.github.com>
2024-12-08 13:41:13 +0800

    Add files via upload

--- javascript2.js
+++ javascript2.js
@@ -1,36 +1,38 @@
-'use strict';
+"use strict";
 
-const modal = document.querySelector('.modal');
-const overlay = document.querySelector('.overlay');
-const btnOpen = document.querySelectorAll('.show-modal');
-const btnClose = document.querySelector('.close-modal');
+//模块使用的强化训练
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnOpen = document.querySelectorAll(".show-modal");
+const btnClose = document.querySelector(".close-modal");
 console.log(btnOpen);
 
 //打开模块
 const openModal = function () {
-  console.log('I click');
-  modal.classList.remove('hidden');
-  overlay.classList.remove('hidden');
+  console.log("I click");
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
 };
 
 //关闭模块
 const closeModal = function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
 };
 
 for (let i = 0; i < btnOpen.length; i++)
-  btnOpen[i].addEventListener('click', openModal);
+  btnOpen[i].addEventListener("click", openModal);
 
-btnClose.addEventListener('click', closeModal);
-overlay.addEventListener('click', closeModal);
+btnClose.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
 
 //使用按键退出模块
-document.addEventListener('keydown', function (e) {
+document.addEventListener("keydown", function (e) {
   console.log(e.key);
 
-  if (e.key === 'Escape') {
-    if (!modal.classList.contains('hidden')) {
+  if (e.key === "Escape") {
+    if (!modal.classList.contains("hidden")) {
       closeModal();
     }
   }
