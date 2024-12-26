088b6fee7fd367cb51881e1a930e12e49f67abec
imokokok <145034722+imokokok@users.noreply.github.com>
2024-12-26 23:50:39 +0800

    Add files via upload

---
+++ in-depth/script3.js
@@ -0,0 +1,29 @@
+'use strict';
+// 构造函数
+function Person(name) {
+  this.name = name;
+}
+
+Person.prototype.sayHello = function () {
+  console.log(`Hello, my name is ${this.name}.`);
+};
+
+// 类
+class Employee {
+  constructor(name, jobTitle) {
+    Person.call(this, name); // 调用构造函数来初始化属性
+    this.jobTitle = jobTitle;
+  }
+
+  sayHello() {
+    console.log(`Hello, my name is ${this.name} and I am a ${this.jobTitle}.`);
+  }
+}
+
+// 设置原型链继承
+Employee.prototype = Object.create(Person.prototype);
+Employee.prototype.constructor = Employee; // 修复constructor属性
+
+// 创建实例
+const employee = new Employee('Imok', 'Developer');
+employee.sayHello(); // 输出：Hello, my name is Imok and I am a Developer.
