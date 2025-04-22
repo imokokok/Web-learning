c07af1a006ec112076948ce3b9cc08e0269d03bc
imokokok <145034722+imokokok@users.noreply.github.com>
2025-04-22 12:01:57 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+//const person: object{name:string,age:number} = {
+var person = {
+    name: "imokokok",
+    age: 20,
+    hobbies: ["Reading", "Watching movies"],
+};
+var favoriteActivities;
+favoriteActivities = ["Sport"];
+console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby);
+}
